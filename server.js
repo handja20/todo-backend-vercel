4545bb963d254aee05310fe25e9859645d3d43dc
handja20 <108028667+handja20@users.noreply.github.com>
2025-06-17 09:47:52 +0200

    Port removed

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen( () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
